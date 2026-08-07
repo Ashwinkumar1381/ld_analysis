c55cb1cd1cbc1bb9fd7e8d70827e991bd8e3b1c5
ethaya_lab_C1 <ch23s006@smail.iitm.ac.in>
2026-08-07 22:53:50 +0530

    ---------- 07.08.26 ----------- *) Modifications to binary_collisions.cpp to allow the calculation of velocity correlations before and after collisions

--- traj/binary_collisions.cpp
+++ traj/binary_collisions.cpp
@@ -5,19 +5,19 @@ using namespace analysis;
 
 int main(int argc, char *argv[])
 {
-	float Lx = 10.0, Ly = 10.0;
+	float Lx = 100.0, Ly = 100.0;
 	int nAtomTypes = 2;
 
-	int frameStart = int(0), frameEnd = int(2e3);
+	int frameStart = int(0), frameEnd = int(5e3);
 	float dt = 5e-4;
-	int frameW = int(100);
+	int frameW = int(10);
+
+	int run_nr = 0;
+	sscanf(argv[1], "%d", &run_nr);
 
 	Trajectory *TRAJ = new Trajectory(dt, frameW, "cfg");
 	sprintf(TRAJ->fpathI, "//run/media/ethaya_lab_c1/ASH_DRIVE_3/ashwin_md/binary_collisions/tests/traj.cfg");
 	sprintf(TRAJ->fpathO, "//run/media/ethaya_lab_c1/ASH_DRIVE_3/ashwin_md/binary_collisions/tests/collision_stats.dat");
-
-	int run_nr = 0;
-	sscanf(argv[1], "%d", &run_nr);
 
 	TRAJ -> openTrajectory();
 
@@ -34,9 +34,11 @@ int main(int argc, char *argv[])
 
 	TRAJ -> loadTrajectory(ATOMS, BOX, frameStart, frameEnd);
 
-	int nFrames = 0;
+	float init_pos_x = 0.0, init_pos_y = 0.0;
 	float dely_init = 0.0, dist_x = 0.0, dist_y = 0.0, disp_x = 0.0, disp_y = 0.0;
-	bool track = false;
+
+	bool pos_track = false, vel_track = true;
+	int nFrames = 0, tau_col_frames = 0;
 
 	float *u_init = new float[2];
 	float *u_final = new float[2];
@@ -51,7 +53,40 @@ int main(int argc, char *argv[])
 		float dxij = ATOMS[currFrame][1].rxt1 - ATOMS[currFrame][0].rxt1 + BOX->Lx*(ATOMS[currFrame][1].jumpx - ATOMS[currFrame][0].jumpx);
 		float dyij = ATOMS[currFrame][1].ryt1 - ATOMS[currFrame][0].ryt1 + BOX->Ly*(ATOMS[currFrame][1].jumpy - ATOMS[currFrame][0].jumpy);
 
-		if(track == true)
+		if(pos_track == false and vel_track == true)
+		{
+			if(currFrame == 0)
+			{
+				init_pos_x = ATOMS[currFrame][0].rxt1 + BOX->Lx*ATOMS[currFrame][0].jumpx;
+				init_pos_y = ATOMS[currFrame][0].ryt1 + BOX->Ly*ATOMS[currFrame][0].jumpy;
+			}
+
+			if(currFrame != 0)
+			{
+				float curr_pos_x = ATOMS[currFrame][0].rxt1 + BOX->Lx*ATOMS[currFrame][0].jumpx;
+				float curr_pos_y = ATOMS[currFrame][0].ryt1 + BOX->Ly*ATOMS[currFrame][0].jumpy;
+				nFrames += 1;
+
+				u_init[0] += float((curr_pos_x - init_pos_x)/(nFrames*frameW*dt));
+				u_init[1] += float((curr_pos_y - init_pos_y)/(nFrames*frameW*dt));
+			}
+
+			if(dxij < 1.0 and abs(dyij) < 1.0)
+			{
+				pos_track = true;
+				vel_track = false;
+
+				u_init[0] /= nFrames;
+				u_init[1] /= nFrames;
+				nFrames = 0;
+
+				disp_x = ATOMS[currFrame][0].rxt1 + BOX->Lx*ATOMS[currFrame][0].jumpx;
+				disp_y = ATOMS[currFrame][0].ryt1 + BOX->Ly*ATOMS[currFrame][0].jumpy;
+				dely_init = dyij;
+			}
+		}
+
+		if(pos_track == true and vel_track == false)
 		{
 			float dxTracer = ATOMS[currFrame][0].rxt1 - ATOMS[currFrame - 1][0].rxt1 + BOX->Lx*(ATOMS[currFrame][0].jumpx - ATOMS[currFrame - 1][0].jumpx);
 			float dyTracer = ATOMS[currFrame][0].ryt1 - ATOMS[currFrame - 1][0].ryt1 + BOX->Ly*(ATOMS[currFrame][0].jumpy - ATOMS[currFrame - 1][0].jumpy);
@@ -60,21 +95,39 @@ int main(int argc, char *argv[])
 			dist_y += abs(dyTracer);
 			nFrames += 1;
 
-			if(dxij < 0.0 and abs(dyij) >= 1.0)
+			if(abs(dyij) >= 1.0)
 			{
-				track = false;
+				vel_track = true;
 
 				disp_x = ATOMS[currFrame][0].rxt1 + BOX->Lx*ATOMS[currFrame][0].jumpx - disp_x;
 				disp_y = ATOMS[currFrame][0].ryt1 + BOX->Ly*ATOMS[currFrame][0].jumpy - disp_y;
-				float tau_col = float(nFrames*frameW*dt);
+				tau_col_frames = nFrames;
+				nFrames = 0;
 
-				float vx_Tracer = ATOMS[currFrame][0].vx;
-				float vy_Tracer = ATOMS[currFrame][0].vy;
-				float vel_Tracer = sqrt(vx_Tracer*vx_Tracer+vy_Tracer*vy_Tracer);
-				u_final[0] = float(vx_Tracer/vel_Tracer);
-				u_final[1] = float(vy_Tracer/vel_Tracer);
+				init_pos_x = ATOMS[currFrame][0].rxt1 + BOX->Lx*ATOMS[currFrame][0].jumpx;
+				init_pos_y = ATOMS[currFrame][0].ryt1 + BOX->Ly*ATOMS[currFrame][0].jumpy;
+			}
+		}
 
-				float vel_corr = u_init[0]*u_final[0] + u_init[1]*u_final[1];
+		if(pos_track == true and vel_track == true)
+		{
+			float curr_pos_x = ATOMS[currFrame][0].rxt1 + BOX->Lx*ATOMS[currFrame][0].jumpx;
+			float curr_pos_y = ATOMS[currFrame][0].ryt1 + BOX->Ly*ATOMS[currFrame][0].jumpy;
+			nFrames += 1;
+
+			u_final[0] += float((curr_pos_x - init_pos_x)/(nFrames*frameW*dt));
+			u_final[1] += float((curr_pos_y - init_pos_y)/(nFrames*frameW*dt));
+
+			if(nFrames == int(2*tau_col_frames))
+			{
+				u_final[0] /= nFrames;
+				u_final[1] /= nFrames;
+
+				float vel_init = sqrt(u_init[0]*u_init[0] + u_init[1]*u_init[1]);
+				float vel_final = sqrt(u_final[0]*u_final[0] + u_final[1]*u_final[1]);
+				float vel_corr = (u_init[0]*u_final[0] + u_init[1]*u_final[1])/(vel_init*vel_final);
+
+				float tau_col = float(tau_col_frames*frameW*dt);
 
 				fprintf(TRAJ->fileO, "\n%d %g %g %g %g %g %g %g", run_nr, tau_col, dely_init, dist_x, dist_y, disp_x, disp_y, vel_corr);
 
@@ -82,23 +135,6 @@ int main(int argc, char *argv[])
 
 				TRAJ->closeTrajectory(true, true);
 				break;
-			}
-		}
-
-		if(track == false)
-		{
-			if(dxij < 1.0 and abs(dyij) < 1.0)
-			{
-				track = true;
-				disp_x = ATOMS[currFrame][0].rxt1 + BOX->Lx*ATOMS[currFrame][0].jumpx;
-				disp_y = ATOMS[currFrame][0].ryt1 + BOX->Ly*ATOMS[currFrame][0].jumpy;
-				dely_init = dyij;
-
-				float vx_Tracer = ATOMS[currFrame][0].vx;
-				float vy_Tracer = ATOMS[currFrame][0].vy;
-				float vel_Tracer = sqrt(vx_Tracer*vx_Tracer+vy_Tracer*vy_Tracer);
-				u_init[0] = float(vx_Tracer/vel_Tracer);
-				u_init[1] = float(vy_Tracer/vel_Tracer);
 			}
 		}
 	}
