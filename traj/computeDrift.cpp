47d099e1b0f3ba2c99efdbcf9f4971ecc14d9716
ethaya_lab_C1 <ch23s006@smail.iitm.ac.in>
2026-08-31 18:52:23 +0530

    Serial implementation of computeDrift.cpp

--- traj/computeDrift.cpp
+++ traj/computeDrift.cpp
@@ -30,40 +30,28 @@ int main(int argc, char *argv[])
 	float drift_vel = 0.0;
 	long nSamples = long(0);
 
-	// #pragma omp parallel num_threads(1)
+	for(int currFrame = 0; currFrame < TRAJ->totalFrames; currFrame++)
 	{
-		float partial_sum = 0.0;
-		long partial_count = 0;
+		printf("Processing frame %d\n", currFrame);
 
-		// #pragma omp for
-		for(int currFrame = 0; currFrame < TRAJ->totalFrames; currFrame++)
+		int nextFrame = currFrame + 1;
+
+		if(nextFrame < TRAJ->totalFrames)
 		{
-			if(currFrame%10 == 0)
+			for(int i = 0; i < TRAJ->nAtoms; i++)
 			{
-				// printf("Processing frame %d\n", currFrame);
+				float dx = ATOMS[nextFrame][i].rxt1 - ATOMS[currFrame][i].rxt1 + BOX->Lx*(ATOMS[nextFrame][i].jumpx - ATOMS[currFrame][i].jumpx);
+				float delt = float((nextFrame - currFrame)*frameW*dt);
 
-				for(int nextFrame = currFrame + 1; nextFrame < TRAJ->totalFrames; nextFrame++)
-				{
-					for(int i = 0; i < TRAJ->nAtoms; i++)
-					{
-						float dx = ATOMS[nextFrame][i].rxt1 - ATOMS[currFrame][i].rxt1 + BOX->Lx*(ATOMS[nextFrame][i].jumpx - ATOMS[currFrame][i].jumpx);
-						float delt = float((nextFrame - currFrame)*frameW*dt);
-
-						partial_sum += float((dx*dx)/(delt*delt));
-						partial_count++;
-					}
-				}
+				drift_vel += float((dx*dx)/(delt*delt));
+				nSamples++;
 			}
-		}
-
-		// #pragma omp critical
-		{
-			drift_vel += partial_sum;
-			nSamples += partial_count;
 		}
 	}
 
 	drift_vel /= nSamples;
+	drift_vel = sqrt(drift_vel);
+
 	printf("\nDrift velocity averaged over %ld samples, Vd = %f\n", nSamples, drift_vel);
 
 	TRAJ -> closeTrajectory();
